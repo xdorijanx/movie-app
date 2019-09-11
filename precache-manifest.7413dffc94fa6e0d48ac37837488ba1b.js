c4a494d0ee3a844a280f039b8d44255476fa5b06
xdorijanx <dorijan11@gmail.com>
2019-09-11 22:09:16 +0200

    Updates

---
+++ precache-manifest.7413dffc94fa6e0d48ac37837488ba1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3aa655bb8b8aefcaa1aedab5e6171c3",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "39beb19d22c147c71e4b",
+    "url": "/movie-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c43e9318b656b8f40653",
+    "url": "/movie-app/static/js/2.f78dbfe8.chunk.js"
+  },
+  {
+    "revision": "39beb19d22c147c71e4b",
+    "url": "/movie-app/static/js/main.f68bc1e7.chunk.js"
+  },
+  {
+    "revision": "13b3814404106cd2c8df",
+    "url": "/movie-app/static/js/runtime~main.aaa966b3.js"
+  }
+]);
