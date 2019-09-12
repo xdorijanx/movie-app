9d11e010455a7a317c758df9b7e8387c6702c5ec
xdorijanx <dorijan11@gmail.com>
2019-09-12 17:03:15 +0200

    routing established

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,55 @@
-import React from 'react'
-import MovieList from '../MovieList/MovieList'
-import ButtonGroup from '../../components/ButtonGroup/ButtonGroup'
-import MovieModal from '../../components/MovieModal/MovieModal'
-const Layout = ({ data, loading,loadMoreMovies, toggleModal, open, genre, handleGenreChange, searchByGenre}) => {
-    return (
-        <div>
-            <MovieList data={data} />
-            <ButtonGroup loadMoreMovies={loadMoreMovies} loading={loading} toggleModal={toggleModal} />
-            <MovieModal open={open} toggleModal={toggleModal} genre={genre} handleGenreChange={handleGenreChange} searchByGenre={searchByGenre}/>
-        </div>
-    )
-}
+import React from "react";
+import MovieList from "../MovieList/MovieList";
+import ButtonGroup from "../../components/ButtonGroup/ButtonGroup";
+import MovieModal from "../../components/MovieModal/MovieModal";
+import MovieDetails from "../../components/MovieDetails/MovieDetails";
+import { HashRouter, Route, Link } from "react-router-dom";
+const Layout = ({
+  data,
+  loading,
+  loadMoreMovies,
+  toggleModal,
+  open,
+  genre,
+  handleGenreChange,
+  searchByGenre
+}) => {
+  return (
+    <div>
+      <HashRouter>
+        <Route exact path="/" render={() => <MovieList data={data} />} />
 
-export default Layout
+        <Route
+          path="/"
+          exact
+          render={() => (
+            <ButtonGroup
+              loadMoreMovies={loadMoreMovies}
+              loading={loading}
+              toggleModal={toggleModal}
+            />
+          )}
+        />
+        <Route
+          path="/"
+          exact
+          render={() => (
+            <MovieModal
+              open={open}
+              toggleModal={toggleModal}
+              genre={genre}
+              handleGenreChange={handleGenreChange}
+              searchByGenre={searchByGenre}
+            />
+          )}
+        />
+
+        <Route path="/:id" exact>
+          <MovieDetails />
+        </Route>
+      </HashRouter>
+    </div>
+  );
+};
+
+export default Layout;
--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -3,7 +3,7 @@ import React from 'react'
 const MovieDetails = () => {
     return (
         <div>
-            
+            <h1>This is movie details component</h1>
         </div>
     )
 }
