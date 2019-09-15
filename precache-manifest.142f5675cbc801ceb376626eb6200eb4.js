8ee8c082b34cba446d6b4dc8640c9aa9ab0bf9f4
xdorijanx <dorijan11@gmail.com>
2019-09-15 16:17:37 +0200

    Updates

--- precache-manifest.142f5675cbc801ceb376626eb6200eb4.js
+++ precache-manifest.142f5675cbc801ceb376626eb6200eb4.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f93f38a0332ad012fea20dce6636f668",
+    "revision": "ef1fd7a3d15a8383d0b675f3ee4a4a51",
     "url": "/movie-app/index.html"
   },
   {
-    "revision": "2a18033e2f53b16cd1d5",
+    "revision": "cc97db0a42af85bb4a8f",
     "url": "/movie-app/static/css/main.2cce8147.chunk.css"
   },
   {
-    "revision": "108fc02b5965eb2cc262",
-    "url": "/movie-app/static/js/2.ca16dcf9.chunk.js"
+    "revision": "fdea1cb13e0b8ad809af",
+    "url": "/movie-app/static/js/2.f1857dbc.chunk.js"
   },
   {
-    "revision": "2a18033e2f53b16cd1d5",
-    "url": "/movie-app/static/js/main.0a93bb28.chunk.js"
+    "revision": "cc97db0a42af85bb4a8f",
+    "url": "/movie-app/static/js/main.e737289d.chunk.js"
   },
   {
     "revision": "13b3814404106cd2c8df",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie-app/precache-manifest.df8b7227ab015d4bbc151207b5326ae8.js"
+  "/movie-app/precache-manifest.142f5675cbc801ceb376626eb6200eb4.js"
 );
 
 self.addEventListener('message', (event) => {
