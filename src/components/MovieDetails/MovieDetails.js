9bd97db60cc284718454f99fde6d6248872a95ed
xdorijanx <dorijan11@gmail.com>
2019-09-15 14:05:39 +0200

    responsive details layout done

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -5,14 +5,33 @@ import {
   Box,
   Tooltip,
   withStyles,
-  CardMedia
+  Card,
+  CardMedia,
+  CardContent,
+  CardActionArea
 } from "@material-ui/core";
 import Rating from "@material-ui/lab/Rating";
 import humanizeList from "humanize-list";
 const styles = theme => ({
-  paper: {
-    width: "100%",
-    height: "100%"
+  card: {
+    paddingTop: "1vh",
+    minHeight: "100vh",
+    width: "95vw",
+    margin: "0 auto",
+    boxShadow: "none",
+    textAlign: "left"
+  },
+
+  rating_container: {
+    display: "flex",
+    alignItems: "center",
+    width: "50vw",
+    justifyContent: "space-between",
+    [theme.breakpoints.down("sm")]: {
+      flexFlow: "column wrap",
+      width: "100vw",
+      fontSize: "x-large"
+    }
   }
 });
 
@@ -21,34 +40,44 @@ const MovieDetails = ({ classes, movieDetails }) => {
   return (
     <div>
       {movieDetails !== null ? (
-        <Paper>
+        <Card className={classes.card}>
           <div>
-            <Typography variant="h1">
-              Movie Name: {movieDetails.name}
-            </Typography>
+            <Typography variant="h4" gutterBottom>{movieDetails.name}</Typography>
             <CardMedia
               image={`https://image.tmdb.org/t/p/original/${movieDetails.image}`}
-              style={{ height: "60vh", width: "95vw" }}
+              style={{ height: "80vh", width: "95vw" }}
             />
           </div>
-          <div>
-            <Typography>Rating:{movieDetails.rating}</Typography>
-            <Rating
-              name="hover-tooltip"
-              max={10}
-              value={movieDetails.rating}
-              precision={0.5}
-            />
-          </div>
-          <div>
+          <CardActionArea></CardActionArea>
+          <CardContent>
+            <div className={classes.rating_container}>
+              <Typography>
+                <strong>Popularity : </strong>
+                {movieDetails.rating}
+              </Typography>
+              <Rating
+                size="medium"
+                name="hover-tooltip"
+                max={10}
+                value={movieDetails.rating}
+                precision={0.5}
+              />
+            </div>
+
             <Typography>
-              Production Companies:{" "}
-              {humanizeList(movieDetails.productionCompanies.map(company => {
-                return company.name;
-              }))}
+              <strong>Language: </strong>
+              {movieDetails.language}
             </Typography>
-          </div>
-        </Paper>
+            <Typography>
+              <strong> Production Companies:</strong>{" "}
+              {humanizeList(
+                movieDetails.productionCompanies.map(company => {
+                  return company.name;
+                })
+              )}
+            </Typography>
+          </CardContent>
+        </Card>
       ) : (
         <Typography>No data available</Typography>
       )}
